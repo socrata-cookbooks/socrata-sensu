10eee5f7bdec89b5cdb521038c602f1b55b6b244
Jonathan Hartman <j@p4nt5.com>
2016-03-29 15:43:04 -0700

    Add source_url and issues_url to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,17 @@
 # Encoding: UTF-8
 
-name             'socrata-sensu'
-maintainer       'Jonathan Hartman'
+name 'socrata-sensu'
+maintainer 'Jonathan Hartman'
 maintainer_email 'jonathan.hartman@socrata.com'
-license          'apache2'
-description      'Installs/Configures socrata-sensu'
+license 'apache2'
+description 'Installs/Configures socrata-sensu'
 long_description 'Installs/Configures socrata-sensu'
-version          '0.0.1'
+version '0.0.1'
 
-depends          'sensu', '~> 2.11'
-depends          'runit', '~> 1.7'
+source_url 'https://github.com/socrata-cookbooks/socrata-sensu'
+issues_url 'https://github.com/socrata-cookbooks/socrata-sensu/issues'
 
-supports         'ubuntu'
-supports         'debian'
-supports         'centos'
-supports         'redhat'
-supports         'fedora'
-supports         'amazon'
+depends 'sensu', '~> 2.11'
+depends 'runit', '~> 1.7'
+
+supports 'ubuntu'
